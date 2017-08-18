e7336f5a7036b9277dac6bf6a30a2586bb2ba820
qkstnrshfmswk <cjcj558@naver.com>
2017-08-18 14:44:32 +0900

    fix error

--- routes/museum.js
+++ routes/museum.js
@@ -5,61 +5,61 @@ var db = mongo('museumdb', ['museumInfo']);
 
 
 
-// router.get('/', function(req, res, next){
-//     db.museumInfo.find({}, function(err, doc){
-//         if(err) res.send(err);
-//         res.json(doc);
-//     });
-// });
+router.get('/', function(req, res, next){
+    db.museumInfo.find({}, function(err, doc){
+        if(err) res.send(err);
+        res.json(doc);
+    });
+});
 
-// router.post('/', function(req, res) {
-//   var info_img = req.body.info_img;
-//   var info = req.body.info;
+router.post('/', function(req, res) {
+  var info_img = req.body.info_img;
+  var info = req.body.info;
   
-//   db.museumInfo.insert(
-//         {
-//             info_img : info_img,
-//             info : info
-//         },
-//         function(err, doc){
-//           if(err) res.send(err);
-//           res.json(doc);
-//         }
-//     )
-// });
+  db.museumInfo.insert(
+        {
+            info_img : info_img,
+            info : info
+        },
+        function(err, doc){
+          if(err) res.send(err);
+          res.json(doc);
+        }
+    )
+});
 
-// router.put('/:_id', function(req, res, next){
-//   var id = req.params._id;
-//   var info_img = req.body.info_img;
-//   var info = req.body.info;
+router.put('/:_id', function(req, res, next){
+  var id = req.params._id;
+  var info_img = req.body.info_img;
+  var info = req.body.info;
   
-//   db.museumInfo.update(
-//     {
-//       _id:mongo.ObjectId(id)
-//     },{
-//       $set : {  
-//         info_img : info_img,
-//         info : info
+  db.museumInfo.update(
+    {
+      _id:mongo.ObjectId(id)
+    },{
+      $set : {  
+        info_img : info_img,
+        info : info
 
-//       }
-//     }, {upset : false},
-//     function (err, doc){
-//       if(err) res.send(err);
-//       res.json(doc);
-//     }
-//   )
-// });
+      }
+    }, {upset : false},
+    function (err, doc){
+      if(err) res.send(err);
+      res.json(doc);
+    }
+  )
+});
 
-// router.delete('/:_id',function (req,res) {
-//     id = req.params._id;
+router.delete('/:_id',function (req,res) {
+    id = req.params._id;
  
-//     db.museumInfo.remove(
-//         {
-//             _id:mongo.ObjectId(id)
-//         }, function (err,doc) {
-//             res.json(doc);
-//         }
-//     )
-// });
+    db.museumInfo.remove(
+        {
+            _id:mongo.ObjectId(id)
+        }, function (err,doc) {
+            res.json(doc);
+        }
+    )
+});
 
-// module.exports = router;
+ module.exports = router;
